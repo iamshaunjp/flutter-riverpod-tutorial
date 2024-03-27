0bf67206423c467c818661357097a89048433a55
Shaun <shaun@thenetninja.co.uk>
2024-03-27 15:23:25 +0000

    lesson-3

---
+++ lib/providers/products_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/models/product.dart';
+
+const List<Product> allProducts = [
+  Product(id: '1', title: 'Groovy Shorts', price: 12.99, image: 'assets/products/shorts.png'),
+  Product(id: '2', title: 'Karati Kit', price: 34.99, image: 'assets/products/karati.png'),
+  Product(id: '3', title: 'Denim Jeans', price: 54.99, image: 'assets/products/jeans.png'),
+  Product(id: '4', title: 'Red Backpack', price: 14.99, image: 'assets/products/backpack.png'),
+  Product(id: '5', title: 'Drum & Sticks', price: 29.99, image: 'assets/products/drum.png'),
+  Product(id: '6', title: 'Blue Suitcase', price: 44.99, image: 'assets/products/suitcase.png'),
+  Product(id: '7', title: 'Roller Skates', price: 52.99, image: 'assets/products/skates.png'),
+  Product(id: '8', title: 'Electric Guitar', price: 79.99, image: 'assets/products/guitar.png'),
+];
+
+final productsProvider = Provider<List<Product>>((ref) {
+  return allProducts;
+});
+
+final reducedProductsProvider = Provider<List<Product>>((ref) {
+  return allProducts.where((p) => p.price < 50).toList();
+});
