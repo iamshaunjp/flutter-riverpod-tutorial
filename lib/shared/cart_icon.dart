5cfd0cd3e2abe8cf69bfbb58251351127727ff19
Shaun <shaun@thenetninja.co.uk>
2024-03-28 12:02:41 +0000

    lesson-11

--- lib/shared/cart_icon.dart
+++ lib/shared/cart_icon.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/cart_provider.dart';
 import 'package:riverpod_files/screens/cart/cart_screen.dart';
 
-class CartIcon extends StatelessWidget {
+class CartIcon extends ConsumerWidget {
   const CartIcon({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final numberOfItemsInCart = ref.watch(cartNotifierProvider).length;
+
     return Stack(
       children: [
         IconButton(
@@ -27,6 +31,9 @@ class CartIcon extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
               color: Colors.blueAccent,
             ),
+            child: Text(numberOfItemsInCart.toString(), style: const TextStyle(
+              color: Colors.white,
+            ))
           ),
         ),
       ],
