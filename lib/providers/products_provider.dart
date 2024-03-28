28b64e675901525275338089d27b9560707038b0
Shaun (Net Ninja) <9838872+iamshaunjp@users.noreply.github.com>
2024-03-28 10:44:03 +0000

    Update products_provider.dart

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -2,14 +2,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/models/product.dart';
 
 const List<Product> allProducts = [
-  Product(id: '1', title: 'Groovy Shorts', price: 12.99, image: 'assets/products/shorts.png'),
-  Product(id: '2', title: 'Karati Kit', price: 34.99, image: 'assets/products/karati.png'),
-  Product(id: '3', title: 'Denim Jeans', price: 54.99, image: 'assets/products/jeans.png'),
-  Product(id: '4', title: 'Red Backpack', price: 14.99, image: 'assets/products/backpack.png'),
-  Product(id: '5', title: 'Drum & Sticks', price: 29.99, image: 'assets/products/drum.png'),
-  Product(id: '6', title: 'Blue Suitcase', price: 44.99, image: 'assets/products/suitcase.png'),
-  Product(id: '7', title: 'Roller Skates', price: 52.99, image: 'assets/products/skates.png'),
-  Product(id: '8', title: 'Electric Guitar', price: 79.99, image: 'assets/products/guitar.png'),
+  Product(id: '1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
+  Product(id: '2', title: 'Karati Kit', price: 34, image: 'assets/products/karati.png'),
+  Product(id: '3', title: 'Denim Jeans', price: 54, image: 'assets/products/jeans.png'),
+  Product(id: '4', title: 'Red Backpack', price: 14, image: 'assets/products/backpack.png'),
+  Product(id: '5', title: 'Drum & Sticks', price: 29, image: 'assets/products/drum.png'),
+  Product(id: '6', title: 'Blue Suitcase', price: 44, image: 'assets/products/suitcase.png'),
+  Product(id: '7', title: 'Roller Skates', price: 52, image: 'assets/products/skates.png'),
+  Product(id: '8', title: 'Electric Guitar', price: 79, image: 'assets/products/guitar.png'),
 ];
 
 final productsProvider = Provider((ref) {
